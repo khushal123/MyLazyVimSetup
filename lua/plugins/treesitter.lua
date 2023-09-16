029693ca80718bfc88cbdfc152187f57637c5685
khushal <khushalsingh12@gmail.com>
2023-09-16 23:46:18 +0530

    my lazyvim, but dont use it

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+        "typescript",
+        "tsx",
+        "json",
+        "json5",
+        "jsonc",
+      })
+    end,
+  },
+}
